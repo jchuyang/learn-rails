f929d150b0231349a2d27a44bb3c72b381ff1b32
Jonathan Chuyang <jchuyang@gmail.com>
2015-07-01 16:26:20 -0700

    new files

---
+++ app/models/owner.rb
@@ -0,0 +1,23 @@
+class Owner
+
+  def name
+    name = 'Foobar Kadigan'
+  end
+
+  def birthdate
+    birthdate = Date.new(1990, 12, 22)
+  end
+
+  def countdown
+    Rails.logger.debug 'DEBUG: entering Owner countdown method'
+    today = Date.today
+    Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'DEBUG: Today date is:'
+    Rails.logger.debug today  
+    birthday = Date.new(today.year, birthdate.month, birthdate.day)
+    if birthday > today
+      countdown = (birthday - today).to_i
+    else
+      countdown = (birthday.next_year - today).to_i
+    end
+  end
+end
